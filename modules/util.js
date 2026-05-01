65990c2e126e7f97e6aa26d4fb1772a8ac990e1c
angelomagnoBR <angelomagno@gmail.com>
2026-05-01 13:53:32 -0300

    Update util.js

--- modules/util.js
+++ modules/util.js
@@ -1,3 +1,18 @@
-export const rootStyle = document.querySelector(':root').style;
+// util.js
 
+// Definição manual da função debounce para evitar dependências de bibliotecas externas
+function debounce(callback, wait) {
+  let timerId;
+  return (...args) => {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      callback(...args);
+    }, wait);
+  };
+}
+
+// Acesso ao estilo da raiz do documento para manipulação de variáveis CSS
+export const rootStyle = document.documentElement.style;
+
+// Função para recarregar a página com um pequeno atraso, evitando loops de recarregamento
 export const debouncedReload = debounce(() => window.location.reload(), 100);
